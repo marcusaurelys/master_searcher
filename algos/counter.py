69be819846686f01ca3df91813443df10ac004fa
Joel Ethan Batac <bjoelethan@gmail.com>
2026-03-04 01:16:56 +0800

    initial commit

---
+++ algos/counter.py
@@ -0,0 +1,9 @@
+class Counter:
+    def __init__(self, func):
+        self.count = 0
+        self.func = func
+    
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        return self.func(*args, **kwargs)
+    
---
+++ algos/naive.py
@@ -0,0 +1,28 @@
+import numpy as np
+from algos.counter import Counter
+
+
+
+#compares two patterns
+def naive_compare(x, y, height, width, comp):
+    for i in range(height):
+        for j in range(width):
+            if not comp(x[i, j], y[i, j]):
+                return False
+    return True
+
+def naive_search(text: np.array, pattern: np.array):
+
+    #instantiate comp counter
+    is_equal = Counter(lambda x, y: x==y)
+
+    pattern_height, pattern_width = pattern.shape
+    text_height, text_width = text.shape
+
+    #while pattern is within bound of text
+    for cur_i in range(text_height-pattern_height+1):
+        for cur_j in range(text_width-pattern_width+1):
+            if naive_compare(pattern, text[cur_i:cur_i+pattern_height, cur_j:cur_j+pattern_width], pattern_height, pattern_width, is_equal):
+                return is_equal.count, (cur_i, cur_j)
+
+    return is_equal.count, None
